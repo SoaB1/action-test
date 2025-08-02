616d1371165b2f63ca3b1e73560a124bbbf356ce
SoaB1 <kumachan1230@gmail.com>
2025-08-02 19:38:48 +0900

    refactor: Update providers and variables in Terraform configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,5 @@
 terraform {
   required_providers {
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-      version = "3.16.0"
-    }
-
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
@@ -25,40 +20,11 @@ terraform {
   }
 }
 
-variable "email" {
-  type      = string
-  sensitive = true
+provider "aws" {
+  region = var.aws_region
 }
 
-variable "api_key" {
-  type      = string
-  sensitive = true
+provider "tailscale" {
+  api_key = var.tailscale_api_key
+  tailnet = var.tailscale_tailnet
 }
-
-variable "cloudflare_domain" {
-  type      = string
-  sensitive = true
-}
-
-variable "zone_id" {
-  type      = string
-  sensitive = true
-}
-
-provider "cloudflare" {
-  email   = var.email
-  api_key = var.api_key
-}
-
-data "cloudflare_access_identity_provider" "test_data" {
-  zone_id = var.zone_id
-  name    = "MasayaNetwork"
-}
-
-# resource "cloudflare_access_application" "test_network" {
-#   zone_id          = var.zone_id
-#   name             = "Test Network"
-#   domain           = "*.test-aaa.${var.cloudflare_domain}"
-#   type             = "self_hosted"
-#   session_duration = "24h"
-# }
